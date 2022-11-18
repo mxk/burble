84b0b4a0d8983720827691cb0b71b743aa10a2a6
Maxim Khitrov <mkhitrov@blackrockneuro.com>
2022-11-18 10:50:26 -0500

    Remove BR/EDR C-frame codes

--- src/l2cap/consts.rs
+++ src/l2cap/consts.rs
@@ -1,7 +1,5 @@
 #![allow(clippy::use_self)]
 
-use super::Cid;
-
 /// Basic L2CAP header size ([Vol 3] Part A, Section 3).
 pub(super) const L2CAP_HDR: usize = 4;
 
@@ -18,18 +16,10 @@ pub(super) const L2CAP_HDR: usize = 4;
 )]
 #[non_exhaustive]
 #[repr(u16)]
-pub enum Code {
+pub enum SigCode {
     CommandRejectRsp = 0x01,
-    ConnectionReq = 0x02,
-    ConnectionRsp = 0x03,
-    ConfigurationReq = 0x04,
-    ConfigurationRsp = 0x05,
     DisconnectionReq = 0x06,
     DisconnectionRsp = 0x07,
-    EchoReq = 0x08,
-    EchoRsp = 0x09,
-    InformationReq = 0x0A,
-    InformationRsp = 0x0B,
     ConnectionParameterUpdateReq = 0x12,
     ConnectionParameterUpdateRsp = 0x13,
     LeCreditBasedConnectionReq = 0x14,
@@ -39,36 +29,4 @@ pub enum Code {
     CreditBasedConnectionRsp = 0x18,
     CreditBasedReconfigureReq = 0x19,
     CreditBasedReconfigureRsp = 0x1A,
-}
-
-impl Code {
-    /// Returns whether the command can be used on the specified CID.
-    #[must_use]
-    pub const fn is_allowed_on(self, cid: Cid) -> bool {
-        use Code::*;
-        let (signal, le_signal) = (cid.0 == Cid::SIGNAL.0, cid.0 == Cid::LE_SIGNAL.0);
-        #[allow(clippy::match_same_arms)]
-        match self {
-            CommandRejectRsp => signal || le_signal,
-            ConnectionReq => signal,
-            ConnectionRsp => signal,
-            ConfigurationReq => signal,
-            ConfigurationRsp => signal,
-            DisconnectionReq => signal || le_signal,
-            DisconnectionRsp => signal || le_signal,
-            EchoReq => signal,
-            EchoRsp => signal,
-            InformationReq => signal,
-            InformationRsp => signal,
-            ConnectionParameterUpdateReq => le_signal,
-            ConnectionParameterUpdateRsp => le_signal,
-            LeCreditBasedConnectionReq => le_signal,
-            LeCreditBasedConnectionRsp => le_signal,
-            FlowControlCreditInd => signal || le_signal,
-            CreditBasedConnectionReq => signal || le_signal,
-            CreditBasedConnectionRsp => signal || le_signal,
-            CreditBasedReconfigureReq => signal || le_signal,
-            CreditBasedReconfigureRsp => signal || le_signal,
-        }
-    }
 }
