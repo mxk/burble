5e94400e7b7951805991a1a15bf3160f403a68ef
Maxim Khitrov <mkhitrov@blackrockneuro.com>
2023-02-27 12:42:26 -0500

    Remove Cache::hash method

--- src/gatt/gatt.rs
+++ src/gatt/gatt.rs
@@ -18,8 +18,6 @@ mod server;
 /// Interface to persistent GATT cache storage.
 type CacheStore = dyn crate::PeerStore<Value = Cache>;
 
-// TODO: Remove blake3
-
 /// Per-device cache ([Vol 3] Part G, Section 2.5.2). For bonded devices, the
 /// cache persists across connections.
 #[derive(Clone, Debug, Default, serde::Deserialize, serde::Serialize)]
@@ -27,19 +25,4 @@ type CacheStore = dyn crate::PeerStore<Value = Cache>;
 pub struct Cache {
     db_hash: u128,
     vals: BTreeMap<Handle, Vec<u8>>,
-}
-
-impl Cache {
-    /// Calculates a cryptographic hash of cache contents that can be used to
-    /// detect changes.
-    fn hash(&self) -> blake3::Hash {
-        let mut h = blake3::Hasher::new();
-        h.update(&self.db_hash.to_le_bytes());
-        for (&hdl, v) in &self.vals {
-            h.update(&u16::from(hdl).to_le_bytes());
-            h.update(&v.len().to_le_bytes());
-            h.update(v);
-        }
-        h.finalize()
-    }
 }
